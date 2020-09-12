eb2e4c978e64499718f589bbb7245b799ca7fcb7
Luis Alvarez <lalberto8085@gmail.com>
2020-09-12 10:01:44 -0400

    adding service tests

---
+++ IMC-Test/IMC.Taxes.Services.Tests/TaxServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMC.Taxes.Services.Tests
+{
+    class TaxServiceTests
+    {
+        [Test]
+        public void CanBeCreated()
+        {
+            var mock = new Mock<ITaxCalculator>();
+            var service = new TaxService(mock.Object);
+            Assert.NotNull(service);
+        }
+
+        [Test]
+        public void TaxRatesCallsTaxCalculator()
+        {
+            var mockMethodCalled = false;
+            var mock = new Mock<ITaxCalculator>();
+            mock.Setup(calculator => calculator.TaxRatesForLocation(It.IsAny<TaxLocationInfo>())).Callback(() => mockMethodCalled = true);
+
+            var service = new TaxService(mock.Object);
+            var location = new TaxLocationInfo();
+            service.TaxRatesForLocation(location);
+
+            Assert.IsTrue(mockMethodCalled);
+        }
+    }
+}
