eecacc77ea741a809403134675bf7e932697d723
Luis Alvarez <lalberto8085@gmail.com>
2020-09-12 09:44:11 -0400

    adding tax service

---
+++ IMC-Test/IMC.Taxes.Services/TaxService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMC.Taxes.Services
+{
+    public class TaxService
+    {
+        private readonly ITaxCalculator taxCalculator;
+
+        public TaxService(ITaxCalculator taxCalculator)
+        {
+            this.taxCalculator = taxCalculator;
+        }
+
+        public TaxRatesInfo TaxRatesForLocation(TaxLocationInfo locationInfo)
+        {
+            return taxCalculator.TaxRatesForLocation(locationInfo);
+        }
+    }
+}
