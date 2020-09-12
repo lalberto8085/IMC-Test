4468d7140680f24c7961668a8fefad70e5c1d828
Luis Alvarez <lalberto8085@gmail.com>
2020-09-12 09:32:48 -0400

    adding test project

---
+++ IMC-Test/IMC.Taxes.TaxJar.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace IMC.Taxes.TaxJar.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SanityTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
